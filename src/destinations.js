dbfa801a481c4f0280dad6dadf11db46c306166c
alkerr <john.musleh@gmail.com>
2018-05-31 12:22:15 +0300

    more bugs

---
+++ src/destinations.js
@@ -0,0 +1,188 @@
+destinations = {	
+	"floorm2":[
+		{"name":"C-201","coord":[-67,17],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C-202","coord":[-65,23],"closest_waypoint":null, "hours":null, "services":["Auditoriums"]},
+		{"name":"C-203","coord":[-65,-3],"closest_waypoint":null, "hours":null, "services":["Auditoriums"]},
+		{"name":"D-201","coord":[59,12],"closest_waypoint":null, "hours":null, "services":["Biotechnology Laboratory"]},
+		{"name":"D-204","coord":[59,5],"closest_waypoint":null, "hours":null, "services":["Laboratory admins"]},
+		{"name":"D-253","coord":[50,5],"closest_waypoint":null, "hours":null, "services":["Laboratory Lockers"]},
+		{"name":"D-253","coord":[59,0],"closest_waypoint":null, "hours":null, "services":["Chemistry & Pharmaceutical Laboratory"]},
+		{"name":"D-212","coord":[50,0],"closest_waypoint":null, "hours":null, "services":["Mechanical Engineering Laboratories"]}
+		
+	],
+	"floorm1":[
+		{"name":"C-101","coord":[-66,20],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C-102","coord":[-63,26],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C-103","coord":[-63,17],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C-104","coord":[-63,8],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C-105","coord":[-63,-1],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+        {"name":"Software Lab", "coord":[-18,27], "closest_waypoint":null, "hours":null, "services":[]}
+	],
+	"floor0":[
+		{"name":"Registration and Consulting Center","coord":[-62,26],"closest_waypoint":null, "hours":"Sun - Thu  09:00 - 19:00", "services":
+			[
+			"Dvorit Freak Shim-Tov: Head of Staff ",
+			"Sharon Turjamen: Academic Advisor",
+			"Rotem Rimon: Academic Advisor",
+			"Chirot Chen: Academic Advisor",
+			"Iti Almado: Academic Advisor",
+			"Sharon Moyal: Director"
+			]
+		},
+		{"name":"Administration - Meeting Room","coord":[41,2],"closest_waypoint":null, "hours":null, "services":
+			[
+			"Dr. Rafi Avraham: CEO",
+			"Prof. Roza Azhari: Chief",
+			"Hadas Tzur: Rector",
+			"Avigail Habib: Secretary",
+			"Zahr Fek Bael: Marketing Coordinator",
+			"Sawsan Abu Toameh: Marketing Coordination for the Arabic community",
+			"Ori Paz: Director of Marketing",
+			"Prof. Tamar Raz-Nachom: Rector",
+			"Aia Shalev: Human Resources Coordinator",
+			"Jodi Sarvro: Director of Resource Development",
+			"Iris Shavil: Director of Yididim Association",
+			"Inat Chen: Human Resources Director",
+			"Tal Sharig: Head of Staff",
+			"Meeting Room"
+			]
+		},
+		{"name":"Student Administration","coord":[-62,-13],"closest_waypoint":null, "hours": "Sun: 8:30 - 12:00 / 13:00 - 19:00\n Mon -Thu:8:30 - 12:00 / 13:00 - 15:00",
+			"services":
+			[
+			"Gili Zigdon: Projects Coordinator",
+			"Eden Dar: Assistance and Social Involvement” Section",
+			"Dana Levi: Mechina Coordinator",
+			"Chaya Moked: Chonchot Program Coordinator",
+			"Avial Simga: Coordinator (Mechanic, Electricity & Electronics Engineering)",
+			"Chen Mor Yosef: Coordinator (Advanced Materials, Industry & Management Engineering)",
+			"Bella Ben Shimol: Coordinator (Civil Engineering, Software Engineering (Machar Program), Mechanical Engineering (Intel Program))",
+			"Limor Nasimi: Coordinator (Software, Electricity & Electronics Engineering)",
+			"Rachel Asolon: Administrative Assistant for the Academic Director",
+			"Adi Siso Yorin: Director of the “Assistance and Social Involvement” Section",
+			"Hadar Dar: Head of Student Administration",
+			"Ronit Shoshoni: Exams & Grades Section",
+			"Mayan Israel: Exams & Grades Section",
+			"Yael Hadiya: Finance Section",
+			"Natali Steinberg: Finance Section",
+			"Dina Blostozki: Alumni Association Coordinator and Career Guidance",
+			"Sarit Tsuk: Head of Alumni Association and Career Guidance",
+			"Suzan Wolf: Counselor and Support",
+			"Mira Shem-Tov: Educational Consultant",
+			"Lina Saleh: Coordinator of the Arab society",
+			"Inat Tsbri: Mechina Coordinator",
+			"Neama Lintser: Mechina Director"
+			]
+		},
+		{"name":"Finance Department","coord":[25.5,0.35],"closest_waypoint":null, "hours":null, "services":["Finance Department"]},
+		{"name":"001","coord":[19.8,10.4],"closest_waypoint":null, "hours":null, "services":["Mechina classRoom"]},
+		{"name":"002","coord":[23,17],"closest_waypoint":null, "hours":null, "services":["Mechina classRoom"]},
+		{"name":"003","coord":[23,21],"closest_waypoint":null, "hours":null, "services":["Mechina classRoom"]},
+		{"name":"004","coord":[23,29],"closest_waypoint":null, "hours":null, "services":["Mechina classRoom"]},
+		{"name":"005","coord":[26,28],"closest_waypoint":null, "hours":null, "services":["Mechina classRoom"]},
+		{"name":"006","coord":[26,26],"closest_waypoint":null, "hours":null, "services":["Mechina classRoom"]},
+		{"name":"007","coord":[26,12],"closest_waypoint":null, "hours":null, "services":["Mechina classRoom"]},
+		{"name":"008","coord":[26,10],"closest_waypoint":null, "hours":null, "services":["Mechina classRoom"]},
+		
+		{"name":"D009","coord":[-22,-6],"closest_waypoint":null, "hours":null, "services":["Computers classRoom"]},
+		{"name":"D010","coord":[-33,-6],"closest_waypoint":null, "hours":null, "services":["Computers classRoom"]},
+
+		{"name":"Studying room","coord":[-62,-0.5],"closest_waypoint":null, "hours":null, "services":["studing room"]},
+		{"name":"Aguda","coord":[-62,23],"closest_waypoint":null, "hours":null, "services":["Aguda"]},
+		{"name":"C001","coord":[-64.6,20],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+
+		{"name":"Cafeteria","coord":[-21,3],"closest_waypoint":null, "hours":null, "services":["Mechina classRoom"]}
+
+
+		
+	],
+	"floor1":[
+		{"name":"C101","coord":[-63.5,26],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C102","coord":[-60.5,31],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C103","coord":[-60.5,21],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C104","coord":[-60.5,14],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C105","coord":[-60.5,5.5],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C106","coord":[-60.5,-3],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C107","coord":[-60.5,-11],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C108","coord":[-60.5,-20],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C109","coord":[-60.5,-27],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C110","coord":[-63,-23],"closest_waypoint":null, "hours":null, "services":["Computer classRoom"]},
+		{"name":"D100","coord":[16,9],"closest_waypoint":null, "hours":null, "services":["Auditorium"]},
+		{"name":"D101 - D114","coord":[19,1],"closest_waypoint":null, "hours":null, "services":
+			[
+			"Prof Itan Manor: Head of the Advanced Materials Engineering Department",
+			"Dr. Benjamin Broski: Industrial & Management Engineering",
+			"Dr. Yael Latsder: Advanced Material Engineering",
+			"Dr. Hasin Yehuda: Head of the Software Engineering Department",
+			"Dr. Ben Bar Yosef: Industrial & Management Engineering",
+			"Dr. Guy Keshi: Industrial & Management Engineering",
+			"Prof Avigdor Zangbil: Head of Advanced Materials Engineering Department",
+			"Dr. Rekfat Ofik Almog: Advanced Materials Engineering",
+			"Dr. Penchas Davidov: Industrial & Management Engineering",
+		    "Amin Brodner: Industrial & Management Engineering",
+			"Dr. Asaf Speiner: Software Engineering",
+			"Prof. Yaacov Exman: Head of the Software Engineering M.Sc.Eng Department",
+			"Dr. Roni Horobitz: Head of Industrial & Management Engineering",
+			"Prof Rachel Ben Alyaho Zahri: Software Engineering",
+			"Dr. Rut Spaz: Head of Chemistry Department & CEO of the Teaching Committee",
+			"Shay Tavor: Software Engineering",
+			"Dr. Miriam Allof: Software Engineering",
+			"Meeting Room",
+			"Shimrit Tzur David: Software Engineering",
+			"Dr. Reuvan Igal: Software Engineering"
+			]
+			},
+			{"name":"English & Physics Department","coord":[46,5],"closest_waypoint":null, "hours":null, "services":[null]},
+			{"name":"Atobe","coord":[17,-40],"closest_waypoint":null, "hours":null, "services":[null]}
+
+	],
+	"floor2":[
+		{"name":"Secondary Exit/Entrance","coord":[-44,-40],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C201","coord":[-64,35],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C202","coord":[-60,40],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C203","coord":[-60,32],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C204","coord":[-60,23],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C205","coord":[-60,14],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C206","coord":[-60,5],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C207","coord":[-60,-4],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C208","coord":[-60,-12],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C209","coord":[-60,-15],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C210","coord":[-63,-15],"closest_waypoint":null, "hours":null, "services":["Computer classRoom"]},
+		
+		{"name":"D200","coord":[19,14],"closest_waypoint":null, "hours":null, "services":["Auditorium"]},
+		{"name":"D201","coord":[21,5],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"D202","coord":[21,1],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"D203","coord":[21,-3],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"D204","coord":[21,-15],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"Sport Hall","coord":[-10,-31],"closest_waypoint":null, "hours":null, "services":["sport Hall and Salsa class"]}
+
+	],
+	"floor3":[
+		{"name":"Library","coord":[-62,8],"closest_waypoint":null, 
+		"hours":"Sun:10:00 - 20:00\n Mon 10:00 - 20:00\n Tus:14:00 - 20:00\n Wed:10:00 - 20:00\n Thu:10:00 - 20:00\n Fri:Closed",
+	     "services":["Carol Katz: Librarian ","Books"]
+		},
+		{"name":"C310","coord":[-65,-20],"closest_waypoint":null, "hours":null, "services":["Computer Room"]},
+		{"name":"C302","coord":[-62,36],"closest_waypoint":null, "hours":null, "services":["Computer Room"]},
+		{"name":"C301","coord":[-65,29],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"C307","coord":[-57,-18],"closest_waypoint":null, "hours":null, "services":["Conference Room (Computer Room)"]},
+		{"name":"C306","coord":[-60,-16],"closest_waypoint":null, "hours":null, "services":["Conference Room"]},
+		{"name":"C305","coord":[-60,-17],"closest_waypoint":null, "hours":null, "services":["Conference Room"]},
+		{"name":"C308","coord":[-62,-21],"closest_waypoint":null, "hours":null, "services":["Resting/Sleeping Room"]},
+		
+		{"name":"C358","coord":[-64,-23],"closest_waypoint":null, "hours":null, "services":["Floor 3 & Cleaners manager"]},
+		{"name":"D300","coord":[18,9],"closest_waypoint":null, "hours":null, "services":["Auditorium"]},
+		{"name":"D301","coord":[19,4],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"D302","coord":[19,-3],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"D303","coord":[19,-10],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"D304","coord":[19,-18],"closest_waypoint":null, "hours":null, "services":["classRoom"]},
+		{"name":"Advanced Materials Laboratory","coord":[25,9],"closest_waypoint":null, "hours":null, "services":
+		[
+		"David Avishi: Industrial & Management Laboratories Manager",
+		"VLSI / ASIC: Laboratories",
+		"Electronics Laboratory" ,
+		"Electricity and Electronics Laboratory: Laboratory",
+		"Ana Graph: Electricity and Electronics Laboratories Manager"
+		]}
+	]
+}
